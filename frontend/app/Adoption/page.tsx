2cc37e573405ce4c9bb0d352c1e5bd2d39d8fac9
Aditya Verma <adityakaplish11@gmail.com>
2024-10-07 23:52:48 +0530

    small changes

--- frontend/app/Adoption/page.tsx
+++ frontend/app/Adoption/page.tsx
@@ -4,16 +4,19 @@ import { useSelector } from "../../redux/store/store";
 export default function DonationCard() {
     const state=useSelector();
     return (
-        <div className={`${state? "dark bg-[#212121]":"bg-white"} flex justify-center place-items-center min-h-screen h-fit tracking-wider flex-wrap gap-10 p-10`}>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
-            <Petcard/>
+        <div className={`${state? "dark bg-[#212121]":"bg-white"} min-h-screen h-fit tracking-wider p-10`}>
+            <h1 className="text-3xl font-bold  text-[#F6855B] flex justify-center">Meet The Animals</h1>
+            <h2 className="flex justify-center mt-2 text-4xl font-semibold text-[#414141] dark:text-white"> Waiting for Adoption</h2>
+            <div className="flex justify-center place-items-center gap-10 flex-wrap mt-10">
+                <Petcard name={"Tommy"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog.png"}/>
+                <Petcard name={"Shadow"} breed={"Abyssinian"} sex={"Female"} birth={"2023"} image={"/Cat3.png"}/>
+                <Petcard name={"Jack"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog1.png"}/>
+                <Petcard name={"Jack"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog.png"}/>
+                <Petcard name={"Jack"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog.png"}/>
+                <Petcard name={"Jack"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog.png"}/>
+                <Petcard name={"Jack"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog.png"}/>
+                <Petcard name={"Jack"} breed={"German Shefard"} sex={"Male"} birth={"2022"} image={"/Dog.png"}/>
+            </div>
         </div>
     )
 }
--- frontend/app/components/PetCard.tsx
+++ frontend/app/components/PetCard.tsx
@@ -1,20 +1,20 @@
 "use client"
 import Image from "next/image";
 
-export default function Petcard() {
+export default function Petcard({name,breed,birth,sex,image}:{name:string,breed:string,birth:string,sex:string,image:string}) {
     return (
         <div className="relative border-2 border-[#fbf8c5] rounded-md">
-            <div className="bg-[#FFFDD0] w-fit h-fit p-10 rounded ">
-                <Image src={"/Dog.png"} height={200} width={150} alt="Animals.png" />
+            <div className="bg-[#FFFDD0] h-[280px] w-[250px] p-10 rounded ">
+                <Image src={image} height={300} width={150} alt="Animals.png" className="min-h-[150px] min-w-[150px]" />
             </div>
             <div className="text-black bg-white rounded p-1 absolute bottom-0 w-full">
-                <h1 className="text-2xl font-bold flex justify-center text-[#F6855B]">Jack</h1>
+                <h1 className="text-2xl font-bold flex justify-center text-[#F6855B]">{name}</h1>
                 <div className="flex justify-between">
-                    <h1 className="text-sm">German Shefard</h1>
-                    <h1 className="text-sm">Birth: 2022</h1>
+                    <h1 className="text-sm">{breed}</h1>
+                    <h1 className="text-sm">Birth: {birth}</h1>
                 </div>
                 <div className="flex justify-between mt-1">
-                    <h1 className="text-sm">Male</h1>
+                    <h1 className="text-sm">{sex}</h1>
                     <button className="p-1 pl-4 pr-4 rounded bg-[#F6855B] border-2 border-[#F6855B] hover:bg-transparent hover:text-[#F6855B] text-white text-sm font-medium">Adopt</button>
                 </div>
             </div>
